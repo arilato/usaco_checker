eab921f48c62f448700542654b38062eaa5164a1
Jihan Yin <jihan@Jihans-MacBook-Pro.local>
2017-07-04 11:57:39 -0700

    first commit

---
+++ Academic/astar/filter reports/separateClass.py
@@ -0,0 +1,18 @@
+import numpy as np
+import csv
+import sys
+names = [["adhit", "sankaran"], ["kaylin", "li"], ["nathan", "wei"], ["nolan", "chen"], ["philbert", "fan"], ["samuel", "tian"], ["william", "chon"], ["william", "zhou"], ["alex", "dong"]]
+input = open(sys.argv[1], 'rt')
+output = open(sys.argv[2], 'wt')
+reader = csv.reader(input)
+writer = csv.writer(output, delimiter=',')
+for row in reader:
+    for name in names:
+        #print(name[1])
+        #print(row[0])
+        print(row[0])
+        if name[1] == row[0].lower() and name[0] == row[1].lower():
+            writer.writerow(row)
+            break
+
+
